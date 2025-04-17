e83644c275758d88e222ca0604edf9d3c82dffa3
Matias Orué C <matiasorue@hotmail.com>
2025-04-17 17:55:02 -0300

    chore: add commit message linter configuration

---
+++ .commitlintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "chore",
+        "revert",
+      ],
+    ],
+    "subject-case": [
+      2,
+      "always",
+      ["sentence-case", "start-case", "lower-case"],
+    ],
+  },
+};
