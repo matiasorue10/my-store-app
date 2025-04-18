15204d74b5955a73792a2efc5ec42650024e25aa
Matias Orué C <matiasorue@hotmail.com>
2025-04-18 13:07:24 -0300

    feat: initial setup of redux and redux toolkit

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import "./global.css";
-import { verifyInstallation } from "nativewind";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
-import Login from "./src/login/Login";
+import Login from "./src/features/auth/screens/Login";
+import { store } from "./src/app/store";
+import { Provider } from "react-redux";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
-  verifyInstallation();
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={Login} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
