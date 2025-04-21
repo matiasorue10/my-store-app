f2f1ef6eac742fca3d7258ed72000787e6cc533a
Matias Orué C <matiasorue@hotmail.com>
2025-04-21 11:53:35 -0300

    test: adjust button component test verbiage

--- src/components/ui/button/Button.test.tsx
+++ src/components/ui/button/Button.test.tsx
@@ -2,19 +2,19 @@ import { render, fireEvent } from "@testing-library/react-native";
 import Button from "./Button";
 
 describe("Button component", () => {
-  it("renders the title correctly", () => {
+  it("should render the title correctly", () => {
     const { getByText } = render(<Button title="Click me" />);
     expect(getByText("Click me")).toBeVisible();
   });
 
-  it("applies primary styles by default", () => {
+  it("should apply primary styles by default", () => {
     const { getByText } = render(<Button title="Primary" />);
     const text = getByText("Primary");
 
     expect(text.props.className).toContain("text-white");
   });
 
-  it("handles onPress", () => {
+  it("should handle onPress", () => {
     const onPressMock = jest.fn();
     const { getByRole } = render(
       <Button title="Press me" onPress={onPressMock} />,
