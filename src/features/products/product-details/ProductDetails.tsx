3ff8b9bfd79c3dd68f3d2bf759646c4e42774b68
Matias Orué C <matiasorue@hotmail.com>
2025-04-22 11:19:48 -0300

    feat: implement product details

--- src/features/products/product-details/ProductDetails.tsx
+++ src/features/products/product-details/ProductDetails.tsx
@@ -2,6 +2,10 @@ import { View, Text } from "react-native";
 import React from "react";
 import { RootStackParamList } from "@/src/app/navigation/types/appNavigatorTypes";
 import { RouteProp } from "@react-navigation/native";
+import { useGetProductByIdQuery } from "../services/productsApi";
+import Card from "@/src/components/ui/card/Card";
+import RemoteImage from "@/src/components/ui/remoteImage/RemoteImage";
+import * as Progress from "react-native-progress";
 
 type DetailsRouteProp = RouteProp<RootStackParamList, "ProductDetails">;
 
@@ -11,10 +15,37 @@ type Props = {
 
 const ProductDetails = ({ route }: Props) => {
   const { itemId } = route.params;
-  console.log(itemId);
+  const { data, error, isLoading } = useGetProductByIdQuery(itemId);
+
   return (
     <View>
-      <Text>ProductDetail</Text>
+      <View className="p-1">
+        {error ? (
+          <Text className="text-red-500">
+            Oh no, there was an error. Please retry
+          </Text>
+        ) : isLoading ? (
+          <View className="flex h-screen items-center justify-center">
+            <Progress.Circle
+              size={80}
+              indeterminate={true}
+              testID="progress-indicator"
+            />
+          </View>
+        ) : data ? (
+          <>
+            <View className="p-4">
+              <Card title={data.title} description={data.description}>
+                <View className="flex flex-col justify-center items-center gap-2">
+                  <RemoteImage uri={data.image} />
+                  <Text>Category: {data.category}</Text>
+                  <Text className="font-bold">Price: {data.price}$</Text>
+                </View>
+              </Card>
+            </View>
+          </>
+        ) : null}
+      </View>
     </View>
   );
 };
